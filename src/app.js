d530c510b4b213c6452f22d32d80c3a7a57fbd39
Marek <39656656+mKrupski@users.noreply.github.com>
2018-07-10 17:30:03 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import './assets/scss/app.scss';
 import './assets/js-components/menu.js';
 import './assets/js-components/scroll.js';
+import './assets/js-components/validate.js';
 
 console.log('Its working just fine');
